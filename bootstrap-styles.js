c429e6e1c7b93a4741dc0f6fdefaf12b51c31cf4
xungngo <xungngo@users.noreply.github.com>
2023-04-09 12:49:46 -0400

    Update bootstrap-styles.js

--- bootstrap-styles.js
+++ bootstrap-styles.js
@@ -1,31 +1,20 @@
 import {LitElement, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 class BootstrapStyles extends LitElement {
-  constructor() {
-    super();
-
-    // Check if the BootstrapStyles component has already been added to the document
-    const existingComponent = document.querySelector(`bootstrap-styles`);
-    if (existingComponent) {
-      existingComponent.remove();
-    }
-
-    // Create a new link element and append it to the head of the document
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css';
-    document.head.appendChild(link);
-  }
-
   connectedCallback() {
     super.connectedCallback();
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css');
+    document.head.appendChild(link);
 
-    // Make the component visible, now that it has added the link to the document
-    this.style.display = 'block';
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js');
+    document.body.appendChild(script);
   }
 
   render() {
-    return html``;
+    return html`<slot></slot>`;
   }
 }
 
